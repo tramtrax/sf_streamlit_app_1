918cce64db94fcff9ce6ad2c98015a244cadf3f2
tramtrax <16233487+tramtrax@users.noreply.github.com>
2023-08-08 18:21:24 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('This is the title of the app')
 streamlit.header('And this is a header')
 streamlit.text('Here\'s some text...')
 streamlit.text('... and here\'s some more')
+
+streamlit.text('Here - have some emojis: 🥣 🥗 🐔 🥑🍞')
+
+streamlit.header('🍌🥭 Header with emojis 🥝🍇')
