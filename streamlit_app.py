f4a6a202f05ca2aadc45c3cd4955e99b6c237c2f
tramtrax <16233487+tramtrax@users.noreply.github.com>
2023-08-08 18:18:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('This is the title of the app')
+streamlit.header('And this is the head')
+streamlit.text('Here\'s some text...')
+streamlit.text('... and here\'s some more')
